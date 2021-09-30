6ed816c25952573244dc90fd450e2bb35056ed14
Jo Seonggyu <blueland1998@naver.com>
2021-09-30 17:57:12 +0900

    todoitem 한줄 만드는중

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 import styled from "styled-components";
-
+import { css } from "styled-components";
 import {MdDelete,MdDone} from 'react-icons/md';
 const CheckCircle = styled.div`
-
+    display: flex;
+    flex: 1;
+    border: 2px solid #25F0B7;
+    background-color: white;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    margin: 30px;
 `;
 
-const Text = styled.div``;
+const Text = styled.div`
+    flex: 1;
+    
+    font-size: 20px;
+    
+`;
 
-const Remove = styled.div``;
+const Remove = styled.div`
+    flex: 1;
+    
+`;
 
-function TodoItem({id,text,done}) {
+function TodoItem() {
     return(
         <>
-            <CheckCircle>{MdDone}</CheckCircle>
-            <Text>{text}</Text>
-            <Remove>{MdDelete}</Remove>
+            <CheckCircle><MdDone /></CheckCircle>
+            <Text>열심히 해보자~!</Text>
+            <Remove><MdDelete /></Remove>
         </>
     )
     
